745f2d95bfe127e15d46b91feac264f42c435994
s1030905 <s1030905@outlook.com>
2023-04-13 23:22:15 +0800

    add restaurantSchema

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -6,13 +6,22 @@ const restaurantList = require("../../restaurant.json")
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 db.on("error", () => {
   console.log("mongodb error")
 })
 db.once("open", () => {
   console.log("mongodb connected")
-  console.log(restaurantList)
-  // for (let i = )
+  const restaurants = restaurantList.results
+  for (let i = 0; i < restaurants.length; i++) {
+    Restaurant.create({
+      name: restaurants[i].name,
+      name_en: restaurants[i].name_en,
+      description: restaurants[i].description,
+      location: restaurants[i].location,
+      image: restaurants[i].image
+    })
+  }
+  console.log("done")
 })
